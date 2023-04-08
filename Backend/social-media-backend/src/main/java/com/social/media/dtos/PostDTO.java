fb79bbff647652e9763a109aff6030fb234f6e46
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-08 18:15:02 +0530

    Post module updated.

--- Backend/social-media-backend/src/main/java/com/social/media/dtos/PostDTO.java
+++ Backend/social-media-backend/src/main/java/com/social/media/dtos/PostDTO.java
@@ -2,6 +2,12 @@ package com.social.media.dtos;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,12 +21,18 @@ public class PostDTO {
 
 	private Integer id;
 
+	@Size(min = 1, max = 300)
 	private String content;
 
+	@Column(insertable = false)
+	@CreatedDate
 	private LocalDateTime created_at;
 
+	@Column(insertable = false)
+	@LastModifiedDate
 	private LocalDateTime updated_at;
 
+	@Min(value = 0)
 	private Integer likes;
 
 }
