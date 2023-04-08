08398038e23aee5761a2feff9198d0e831b5d906
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-08 17:25:48 +0530

    Swagger configuration implemented.

--- Backend/social-media-backend/src/main/java/com/social/media/SocialMediaBackendApplication.java
+++ Backend/social-media-backend/src/main/java/com/social/media/SocialMediaBackendApplication.java
@@ -3,7 +3,11 @@ package com.social.media;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Social Media API", version = "1.0"))
 public class SocialMediaBackendApplication {
 
 	public static void main(String[] args) {
