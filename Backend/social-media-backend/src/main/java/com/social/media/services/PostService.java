cf49760c8d8016ec81f85f03008fabefe19118a0
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-08 16:47:28 +0530

    Working on post module services.

---
+++ Backend/social-media-backend/src/main/java/com/social/media/services/PostService.java
@@ -0,0 +1,45 @@
+package com.social.media.services;
+
+import com.social.media.dtos.PostDTO;
+import com.social.media.exceptions.NoRecordFoundException;
+
+public interface PostService {
+
+	/**
+	 * 
+	 * @param postDTO: Takes postDTO object as a parameter and create one new user
+	 *                 in database.
+	 * @return: Returns PostDTO object with unique ID after saving it in database.
+	 */
+	public PostDTO createPost(PostDTO postDTO);
+
+	/**
+	 * 
+	 * @param postId: To find post in database using postId.
+	 * @return: postDTO object retrieved from database.
+	 * @throws NoRecordFoundException: If no post found with given Id then will
+	 *                                 throw NoRecordFoundException.
+	 */
+	public PostDTO getPostById(Integer postId) throws NoRecordFoundException;
+
+	/**
+	 * 
+	 * @param postId:  To find post in database using postId.
+	 * @param postDTO: It contains information about post which needs to be update
+	 *                 in database.
+	 * @return: postDTO object retrieved from database.
+	 * @throws NoRecordFoundException: If no post found with given Id then will
+	 *                                 throw NoRecordFoundException.
+	 */
+	public PostDTO updatePostById(Integer postId, PostDTO postDTO) throws NoRecordFoundException;
+
+	/**
+	 * 
+	 * @param postId: To find post in database using postId.
+	 * @return: String message contains information about deleted operation.
+	 * @throws NoRecordFoundException: If no post found with given Id then will
+	 *                                 throw NoRecordFoundException.
+	 */
+	public String deletePostById(Integer postId) throws NoRecordFoundException;
+
+}
