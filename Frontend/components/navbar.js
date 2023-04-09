f8e830ca989051d2286aec91329727361226a444
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-10 00:29:01 +0530

    Post analytics completed.

--- Frontend/components/navbar.js
+++ Frontend/components/navbar.js
@@ -1,6 +1,5 @@
 let navbar = () => {
   return `<div id="navbar">
-    <h1 id="navbarHeading">Welcome to social media app.</h1>
     <div id="navbarOptions">
       <a href="index.html">Home</a>
       <a href="posts.html">Post</a>
