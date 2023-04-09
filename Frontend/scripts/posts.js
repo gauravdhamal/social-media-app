a306a040ec6e02c61d4720f7cd6220f020b95439
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-09 17:44:46 +0530

    Update user/post implemented.

--- Frontend/scripts/posts.js
+++ Frontend/scripts/posts.js
@@ -4,45 +4,105 @@ document.getElementById("navbar").innerHTML = navbar();
 
 let commonUrl = "http://localhost:8888/";
 
-let postPostForm = document.getElementById("postPostForm");
+// Create post data
+{
+  let postPostForm = document.getElementById("postPostForm");
 
-postPostForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+  postPostForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  let formData = new FormData(event.target);
+    let formData = new FormData(event.target);
 
-  let content = formData.get("content");
+    let content = formData.get("content");
 
-  let postObject = {
-    content: "someContent",
+    let postObject = {
+      content: "someContent",
+    };
+
+    postObject.content = content;
+
+    createPost(postObject);
+  });
+
+  // Create post POST request method.
+  let createPost = (postObject) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postObject),
+    };
+
+    fetch(commonUrl + `posts/`, options)
+      .then((response) => {
+        if (response.status == 201) {
+          return response.json();
+        } else {
+          window.alert("Enter data");
+        }
+      })
+      .then((savedPostObject) => {
+        if (savedPostObject.description == "Validation error") {
+          window.alert(savedPostObject.details);
+        } else {
+          window.alert(`Post created with Id : ${savedPostObject.id}`);
+        }
+        postPostForm.reset();
+      })
+      .catch((error) => console.error("error : ", error));
   };
+}
 
-  postObject.content = content;
+// Update post data
+{
+  let postUpdateForm = document.getElementById("postUpdateForm");
 
-  console.log("postObject:", postObject);
+  postUpdateForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  createPost(postObject);
-});
+    let formData = new FormData(event.target);
 
-// Create post POST request method.
-let createPost = (postObject) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(postObject),
+    let id = formData.get("id");
+    let content = formData.get("content");
+
+    let postObject = {
+      id: 0,
+      content: "someContent",
+    };
+
+    postObject.id = id;
+    postObject.content = content;
+
+    updatePost(postObject);
+  });
+
+  // Update post PUT request method.
+  let updatePost = (postObject) => {
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postObject),
+    };
+
+    fetch(commonUrl + `posts/${postObject.id}`, options)
+      .then((response) => {
+        return response.json();
+      })
+      .then((updatedPostObject) => {
+        if (updatedPostObject.description == "Validation error") {
+          window.alert(updatedPostObject.details);
+        } else if (
+          updatedPostObject.description == `uri=/posts/${postObject.id}`
+        ) {
+          window.alert(updatedPostObject.details);
+        } else {
+          window.alert(`Post updated.`);
+        }
+        postUpdateForm.reset();
+      })
+      .catch((error) => console.error("error : ", error));
   };
-
-  fetch(commonUrl + `posts/`, options)
-    .then((response) => response.json())
-    .then((savedPostObject) => {
-      if (savedPostObject.description == "Validation error") {
-        window.alert(savedPostObject.details);
-      } else {
-        window.alert(`Post created with Id : ${savedPostObject.id}`);
-        console.log("savedPostObject:", savedPostObject);
-      }
-    })
-    .catch((error) => console.error("error : ", error));
-};
+}
--- Frontend/scripts/users.js
+++ Frontend/scripts/users.js
@@ -4,51 +4,107 @@ document.getElementById("navbar").innerHTML = navbar();
 
 let commonUrl = "http://localhost:8888/";
 
-let userPostForm = document.getElementById("userPostForm");
+// Create user data
+{
+  let userPostForm = document.getElementById("userPostForm");
 
-userPostForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+  userPostForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  let formData = new FormData(event.target);
+    let formData = new FormData(event.target);
 
-  let name = formData.get("name");
-  let email = formData.get("email");
-  let bio = formData.get("bio");
+    let name = formData.get("name");
+    let email = formData.get("email");
+    let bio = formData.get("bio");
 
-  let userObject = {
-    name: "someName",
-    email: "someEmail",
-    bio: "someBio",
-  };
+    let userObject = {
+      name: "someName",
+      email: "someEmail",
+      bio: "someBio",
+    };
 
-  userObject.name = name;
-  userObject.email = email;
-  userObject.bio = bio;
+    userObject.name = name;
+    userObject.email = email;
+    userObject.bio = bio;
 
-  createUser(userObject);
-});
+    createUser(userObject);
+  });
 
-// Create user POST request method.
-let createUser = (userObject) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userObject),
-  };
+  // Create user POST request method.
+  let createUser = (userObject) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userObject),
+    };
 
-  fetch(commonUrl + `users/`, options)
-    .then((response) => response.json())
-    .then((savedUserObject) => {
-      if (savedUserObject.description == "Validation error") {
-        window.alert(savedUserObject.details);
-      } else if (savedUserObject.description == "uri=/users/") {
-        window.alert(`Username already exist try with another one.`);
-      } else {
-        window.alert(`User created with Id : ${savedUserObject.id}`);
+    fetch(commonUrl + `users/`, options)
+      .then((response) => response.json())
+      .then((savedUserObject) => {
+        if (savedUserObject.description == "Validation error") {
+          window.alert(savedUserObject.details);
+        } else if (savedUserObject.description == "uri=/users/") {
+          window.alert(`Username already exist try with another one.`);
+        } else {
+          window.alert(`User created with Id : ${savedUserObject.id}`);
+        }
         userPostForm.reset();
-      }
-    })
-    .catch((error) => console.error("error : ", error));
-};
+      })
+      .catch((error) => console.error("error : ", error));
+  };
+}
+
+// Update user data
+{
+  let userUpdateForm = document.getElementById("userUpdateForm");
+
+  userUpdateForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let formData = new FormData(event.target);
+
+    let id = formData.get("id");
+    let updatedName = formData.get("name");
+    let updatedBio = formData.get("bio");
+
+    let userObject = {
+      id: 0,
+      name: "someContent",
+      bio: "someBio",
+    };
+
+    userObject.id = id;
+    userObject.name = updatedName;
+    userObject.bio = updatedBio;
+
+    console.log("userObject:", userObject);
+
+    updateUser(userObject);
+  });
+
+  let updateUser = (userObject) => {
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/Json",
+      },
+      body: JSON.stringify(userObject),
+    };
+
+    fetch(commonUrl + `users/${userObject.id}`, options)
+      .then((response) => response.json())
+      .then((updatedUserObject) => {
+        if (updatedUserObject.description == "Validation error") {
+          window.alert(updatedUserObject.details);
+        } else if (
+          updatedUserObject.description == `uri=/users/${userObject.id}`
+        ) {
+          window.alert(`User not found with Id : ${userObject.id}`);
+        } else {
+          window.alert(`User updated.`);
+        }
+      });
+  };
+}
