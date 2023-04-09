e61722a91a9d19c3690384742b3816d2cce1b918
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-09 16:15:56 +0530

    User creation form completed.

--- Frontend/scripts/users.js
+++ Frontend/scripts/users.js
@@ -1,3 +1,54 @@
 import navbar from "../components/navbar.js";
 
 document.getElementById("navbar").innerHTML = navbar();
+
+let commonUrl = "http://localhost:8888/";
+
+let userPostForm = document.getElementById("userPostForm");
+
+userPostForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let formData = new FormData(event.target);
+
+  let name = formData.get("name");
+  let email = formData.get("email");
+  let bio = formData.get("bio");
+
+  let userObject = {
+    name: "someName",
+    email: "someEmail",
+    bio: "someBio",
+  };
+
+  userObject.name = name;
+  userObject.email = email;
+  userObject.bio = bio;
+
+  createUser(userObject);
+});
+
+// Create user POST request method.
+let createUser = (userObject) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userObject),
+  };
+
+  fetch(commonUrl + `users/`, options)
+    .then((response) => response.json())
+    .then((savedUserObject) => {
+      if (savedUserObject.description == "Validation error") {
+        window.alert(savedUserObject.details);
+      } else if (savedUserObject.description == "uri=/users/") {
+        window.alert(`Username already exist try with another one.`);
+      } else {
+        window.alert(`User created with Id : ${savedUserObject.id}`);
+        userPostForm.reset();
+      }
+    })
+    .catch((error) => console.error("error : ", error));
+};
