e5f019fd64921da293dd56695f42d544d92d0733
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-09 12:48:33 +0530

    Working on frontend part layout completed.

---
+++ Frontend/components/navbar.js
@@ -0,0 +1,12 @@
+let navbar = () => {
+  return `<div id="navbar">
+    <h1 id="navbarHeading">Welcome to social media app.</h1>
+    <div id="navbarOptions">
+      <a href="index.html">Home</a>
+      <a href="posts.html">Post</a>
+      <a href="users.html">User</a>
+    </div>
+  </div>`;
+};
+
+export default navbar;
