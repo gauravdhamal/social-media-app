36a8cd508bd8d64fc47d760f8c29c59d38848709
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-09 10:26:28 +0530

    Test cases implemented for User and Post modules.

--- Backend/social-media-backend/src/main/java/com/social/media/configurations/Config.java
+++ Backend/social-media-backend/src/main/java/com/social/media/configurations/Config.java
@@ -3,6 +3,7 @@ package com.social.media.configurations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootConfiguration
 public class Config {
@@ -10,6 +11,11 @@ public class Config {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
