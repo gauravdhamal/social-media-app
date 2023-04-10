6bf152c2f1ad48f55ee7c790bee6cb2d68a08cff
gauravdhamal <gauravdhamal11@gmail.com>
2023-04-10 10:03:37 +0530

    Some bug fixed.

--- Frontend/scripts/users.js
+++ Frontend/scripts/users.js
@@ -2,7 +2,8 @@ import navbar from "../components/navbar.js";
 
 document.getElementById("navbar").innerHTML = navbar();
 
-let commonUrl = "http://localhost:8888/";
+// http://localhost:8888/
+let commonUrl = "https://social-media-backend-production-a70f.up.railway.app/";
 
 // Create/Update open close function start
 let createUserButton = document.getElementById("createUserButton");
